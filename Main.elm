e220274ead70e28f8bfc48cca8c18a0b23fd1722
Christian Briones <cwbriones@gmail.com>
2016-03-13 21:49:25 -0700

    clean up the view

--- Main.elm
+++ Main.elm
@@ -22,27 +22,40 @@ init = ["one", "two", "three"]
 
 view address model =
   let
-    filterItem = \(_, label) -> li [] [text label]
+    filterView = \(url, label) -> li [class "filter"] [a [href url] [text label]]
+
+    filters = List.map filterView
+      [ ("", "All")
+      , ("#/active", "Active")
+      , ("#/completed", "Completed")
+      ]
 
     appFooter = footer [class "footer"]
       [ span [class "todo-count"] [strong [] [text (toString (List.length model))], text " items left"]
-      , ul [class "filters"] (List.map filterItem [("#/", "All"), ("#/Active", "Active"), ("#/completed", "Completed")])
+      , ul [class "filters"] filters
       ]
-    infoFooter = footer [class "footer"]
+
+    infoFooter = footer [class "info"]
       [ p [] [text "Double-click to edit a todo"]
       , p [] [text "Written by Christian Briones"]
       ]
-  in
-    section [class "todoapp"]
-      [ header [class "header"] [h1 [] [text "todos"]]
-      , input [class "new-todo", placeholder "What needs to be done?", autofocus True] []
+
+    todoApp = section [class "todoapp"]
+      [ header [class "header"]
+        [ h1 [] [ text "todos" ]
+        , input [class "new-todo", placeholder "What needs to be done?", autofocus True] []
+        ]
       , section [class "main"] [ul [class "todo-list"] (List.map viewTodo model)]
       , appFooter
+      ]
+  in
+    div []
+      [ todoApp
       , infoFooter
       ]
 
 viewTodo todo =
-  div [class "view"]
+  li [class "view"]
     [ input [class "toggle", type' "checkbox"] []
     , label [] [text "A todo."]
     , button [class "destroy"] []
